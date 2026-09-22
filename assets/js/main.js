4082aa91e3b7fa824ecb38802f8dff11a2008007
135acostatomas-ia <135acostatomas@gmail.com>
2026-09-22 11:55:47 -0300

    Agregar foto de Morcilla Asturiana

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,6 +46,7 @@ const IMAGENES_PRODUCTOS = {
   "Seso": "https://raw.githubusercontent.com/135acostatomas-ia/WebGanadera/main/assets/img/achuras/seso.webp?v=20260922-1",
   "Salchichas viena con piel": "https://raw.githubusercontent.com/135acostatomas-ia/WebGanadera/main/assets/img/embutidos/salchichas-viena-con-piel.webp?v=20260922-1",
   "Chorizos de carne": "https://raw.githubusercontent.com/135acostatomas-ia/WebGanadera/main/assets/img/embutidos/chorizos-de-carne.webp?v=20260922-1",
+  "Morcilla Asturiana": "https://raw.githubusercontent.com/135acostatomas-ia/WebGanadera/main/assets/img/embutidos/morcilla-asturiana.webp?v=20260922-1",
   "Rabo": "https://raw.githubusercontent.com/135acostatomas-ia/WebGanadera/main/assets/img/achuras/rabo.webp?v=20260922-1",
   "Nuggets": "https://raw.githubusercontent.com/135acostatomas-ia/WebGanadera/main/assets/img/productos/nuggets-bandeja.webp?v=20260918-4",
   "Provoletta Parrillera en cazuela": "https://raw.githubusercontent.com/135acostatomas-ia/WebGanadera/main/assets/img/almacen-y-fiambreria/provoletta-parrillera-cazuela.webp?v=20260918-2",
